f2201973ba8b03fab525e9771283b19a969f43de
Tatel <782487339@qq.com>
2018-02-28 16:57:37 +0800

    修改静态资源路径

--- config/index.js
+++ config/index.js
@@ -56,8 +56,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsSubDirectory: 'sstatic',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
